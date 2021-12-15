5b4cbe36720d30b1cbb2eb621f44e512b5414ed8
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2021-12-15 11:54:57 +0200

    Motion sensor

---
+++ liiketunnistin/parametrit.py
@@ -0,0 +1,15 @@
+SSID1 = 'WiFi1'
+SALASANA1 = 'Salari1'
+SSID2 = 'WiFi2'
+SALASANA2 = 'Salari2'
+MQTT_SERVERI = 'ip'
+MQTT_PORTTI = '1883'
+MQTT_KAYTTAJA = 'useri'
+MQTT_SALASANA = 'salari'
+CLIENT_ID = "ESP32-etelaPIR1"
+DHCP_NIMI = "ESP32-etelaPIR"
+PIR_PINNI = 22
+PIR_LIIKE_NOLLAUSAIKA = 5
+AIHE_LIIKETUNNISTIN = 'koti/ulko/etela/pir1'
+NTPPALVELIN = 'pool.ntp.org'
+AIHE_VIRHEET = 'virheet/sensorit/esp32/virhe'

818a36525875ccc10c13a53cdfdb460f13fd0d41
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2022-10-06 14:50:59 +0300

    Added error solutions and checkups

--- esp32-bme280x3 sensors in 2 i2c channels/main.py
+++ esp32-bme280x3 sensors in 2 i2c channels/main.py
@@ -1,20 +1,30 @@
 """
 This script is used for temperature, moisture and pressure measurument.
-2 I2C channels, pins defined in runtimeconfig. Asynchoronous code.
+2 I2C channels, pins defined in the runtimeconfig.json. Asynchoronous code.
 
-First BME280 is set to Pin(22) = SCL and Pin(21) = SDA (I2C1)
-Second BME280 is set to Pin(32) = SCL and Pin(33) = SDA AND SDO is OPEN or GND (I2C2)
-Third BME280 is set to Pin(32) = SCL and Pin(33) = SDA AND SDO is connected to +3.3V! (I2C2)
+First BME280 is set to Pin(22) = SCL and Pin(21) = SDA. SDO is OPEN (first I2C bus)
+Second BME280 is set to Pin(32) = SCL and Pin(33) = SDA. SDO is OPEN or GND (second I2C bus)
+Third BME280 is set to Pin(32) = SCL and Pin(33) = SDA. SDO is connected to +3.3V! (second I2C bus)
 If you need to change pins, do that in the parameters.py
 
-bmes = First BME280 which uses default address 0x76
-bmet = Second BME280 which uses default address 0x76
-bmeu = Third BME280 which uses address 0x77 because SDO is connected to VCC
-Each sensor has MQTT messages defined in runtimeconfig.json
+If sensor is not found during object initialization, it will be skipped. Program continues even without sensors!
+
+Sensor objects:
+- bmes = First BME280 which uses default address 0x76 in I2C bus 1 !! SDO open !!
+- bmet = Second BME280 which uses default address 0x76 in I2C bus 2 !! SDO open !!
+- bmeu = Third BME280 which uses address 0x77 in I2C bus 2. !! SDO is connected to VCC !!
+
+If you like to add fourth sensor, add code as you want. As an example:
+- Optional: Fourth BME280 is set to Pin(32) = SCL and Pin(33) = SDA. SDO is connected to +3.3V! (first I2C bus)
+- Optional: bmev = Fourth BME280 which uses address 0x77 in I2C bus 2 !! SDO is connected to VCC !!
+
+Each sensor has MQTT messages for temperature, moisture and pressure defined in runtimeconfig.json.
+- Optional: you can add altitude and deviation point information from the sensor if you like.
 
 MQTT messages could be sent to Influxdb and from influxdb to Grafana.
 
-First version: 5.10.2022: Jari Hiltunen & Tarmo Hiltunen
+Version: 6.10.2022: Jari Hiltunen & Tarmo Hiltunen
+
 Tested with 3 x BME280 + ESP32-D Wroom (4Mb) + micropython version 1.19.1
 """
 from machine import SoftI2C, Pin, freq, reset
@@ -30,9 +40,13 @@ gc.collect()
 # Globals
 mqtt_up = False
 broker_uptime = 0
+sensor1faulty = False
+sensor2faulty = False
+sensor3faulty = False
 
 try:
     f = open('parameters.py', "r")
+    #  2 x I2C channels and their pins
     from parameters import I2C1_SCL_PIN, I2C1_SDA_PIN, I2C2_SCL_PIN, I2C2_SDA_PIN
     f.close()
 except OSError:  # open failed
@@ -98,6 +112,7 @@ except OSError:
 
 def resolve_date():
     (year, month, mdate, hour, minute, second, wday, yday) = localtime()
+    #  Finnish
     weekdays = ['Ma', 'Ti', 'Ke', 'To', 'Pe', 'La', 'Su']
     summer_march = mktime((year, 3, (14 - (int(5 * year / 4 + 1)) % 7), 1, 0, 0, 0, 0, 0))
     winter_december = mktime((year, 10, (7 - (int(5 * year / 4 + 1)) % 7), 1, 0, 0, 0, 0, 0))
@@ -119,12 +134,26 @@ freq(240000000)
 # Network handshake
 net = WifiNet.ConnectWiFi(SSID1, PASSWORD1, SSID2, PASSWORD2, NTPSERVER, DHCP_NAME, START_WEBREPL, WEBREPL_PASSWORD)
 
-# BME280 sensor
+# BME280 sensors I2C busses
 i2c1 = SoftI2C(scl=Pin(I2C1_SCL_PIN), sda=Pin(I2C1_SDA_PIN))
 i2c2 = SoftI2C(scl=Pin(I2C2_SCL_PIN), sda=Pin(I2C2_SDA_PIN))
-bmes = BmE.BME280(i2c=i2c1)
-bmet = BmE.BME280(i2c=i2c2)  # Channel 2 0x76 address
-bmeu = BmE.BME280(i2c=i2c2, address=0x77)  # Channel 2 0x77 address (SDO set to 3.3V)
+
+try:
+    bmes = BmE.BME280(i2c=i2c1)
+except OSError as e:
+    print("Error: %s - Sensor 1 (bmes) not connected!" % e)
+    sensor1faulty = True
+try:
+    bmet = BmE.BME280(i2c=i2c2)  # Channel 2 0x76 address
+except OSError as e:
+    print("Error: %s - Sensor 2 (bmet) not connected!" % e)
+    sensor2faulty = True
+try:
+    bmeu = BmE.BME280(i2c=i2c2, address=0x77)  # Channel 2 0x77 address (SDO set to 3.3V)
+except OSError as e:
+    print("Error: %s - Sensor 3 (bmeu) not connected!" % e)
+    sensor3faulty = True
+
 
 async def mqtt_up_loop():
     global mqtt_up
@@ -135,7 +164,7 @@ async def mqtt_up_loop():
 
     if net.net_ok is True:
         config['subs_cb'] = update_mqtt_status
-        # config['connect_coro'] = mqtt_subscribe
+        config['connect_coro'] = mqtt_subscribe
         config['ssid'] = net.use_ssid
         config['wifi_pw'] = net.u_pwd
         if DEBUG_SCREEN_ACTIVE == 1:
@@ -143,20 +172,33 @@ async def mqtt_up_loop():
         else:
             MQTTClient.DEBUG = False
         client = MQTTClient(config)
-        await client.connect()
-        mqtt_up = True
+        while mqtt_up is False:
+            await asyncio.sleep(5)
+            try:
+                await client.connect()
+                if client.isconnected() is True:
+                    mqtt_up = True
+            except OSError as e:
+                if DEBUG_SCREEN_ACTIVE == 1:
+                    print("MQTT error: %s" % e)
+                    print("Config: %s" % config)
 
     n = 0
     while True:
-        # await self.mqtt_subscribe()
         await asyncio.sleep(5)
         if DEBUG_SCREEN_ACTIVE == 1:
             print('mqtt-publish', n)
         await client.publish('result', '{}'.format(n), qos=1)
         n += 1
 
+
+async def mqtt_subscribe(client):
+    # If "client" is missing, you get error from line 538 in MQTT_AS.py (1 given, expected 0)
+    # This information is just to check if the broker is up, nothing else!!
+    await client.subscribe('$SYS/broker/uptime', 1)
+
+
 def update_mqtt_status(topic, msg, retained):
-    # Note! relay commands are not asynchronous!
     global broker_uptime
     if DEBUG_SCREEN_ACTIVE == 1:
         print(resolve_date())
@@ -172,6 +214,7 @@ def update_mqtt_status(topic, msg, retained):
             correction = float(msg)
             return correction """
 
+
 async def mqtt_publish_loop():
 
     while True:
@@ -179,26 +222,30 @@ async def mqtt_publish_loop():
             await asyncio.sleep(10)
         else:
             await asyncio.sleep(MQTT_INTERVAL)  # Seconds
-            if bmes.values[0][:-1] is not None:
-                await client.publish(TOPIC_TEMP_1, bmes.values[0][:-1], retain=0, qos=0)
-            if bmes.values[2][:-1] is not None:
-                await client.publish(TOPIC_RH_1, bmes.values[2][:-1], retain=0, qos=0)
-            if bmes.values[1][:-3] is not None:
-                await client.publish(TOPIC_PRESSURE_1, bmes.values[1][:-3], retain=0, qos=0)
-            # I2C channel 2 sensor which SDO is open or set to GND
-            if bmet.values[0][:-1] is not None:
-                await client.publish(TOPIC_TEMP_2, bmet.values[0][:-1], retain=0, qos=0)
-            if bmet.values[2][:-1] is not None:
-                await client.publish(TOPIC_RH_2, bmet.values[2][:-1], retain=0, qos=0)
-            if bmet.values[1][:-3] is not None:
-                await client.publish(TOPIC_PRESSURE_2, bmet.values[1][:-3], retain=0, qos=0)
-            # I2C channel 2 sensor which SDO is open or set to VCC (3.3 or 5V depending sensor type)
-            if bmeu.values[0][:-1] is not None:
-                await client.publish(TOPIC_TEMP_3, bmeu.values[0][:-1], retain=0, qos=0)
-            if bmeu.values[2][:-1] is not None:
-                await client.publish(TOPIC_RH_3, bmeu.values[2][:-1], retain=0, qos=0)
-            if bmeu.values[1][:-3] is not None:
-                await client.publish(TOPIC_PRESSURE_3, bmeu.values[1][:-3], retain=0, qos=0)
+            if not sensor1faulty:
+                if bmes.values[0][:-1] is not None:
+                    await client.publish(TOPIC_TEMP_1, bmes.values[0][:-1], retain=0, qos=0)
+                if bmes.values[2][:-1] is not None:
+                    await client.publish(TOPIC_RH_1, bmes.values[2][:-1], retain=0, qos=0)
+                if bmes.values[1][:-3] is not None:
+                    await client.publish(TOPIC_PRESSURE_1, bmes.values[1][:-3], retain=0, qos=0)
+            if not sensor2faulty:
+                # I2C channel 2 sensor which SDO is open or set to GND
+                if bmet.values[0][:-1] is not None:
+                    await client.publish(TOPIC_TEMP_2, bmet.values[0][:-1], retain=0, qos=0)
+                if bmet.values[2][:-1] is not None:
+                    await client.publish(TOPIC_RH_2, bmet.values[2][:-1], retain=0, qos=0)
+                if bmet.values[1][:-3] is not None:
+                    await client.publish(TOPIC_PRESSURE_2, bmet.values[1][:-3], retain=0, qos=0)
+            if not sensor3faulty:
+                # I2C channel 2 sensor which SDO is open or set to VCC (3.3 or 5V depending sensor type)
+                if bmeu.values[0][:-1] is not None:
+                    await client.publish(TOPIC_TEMP_3, bmeu.values[0][:-1], retain=0, qos=0)
+                if bmeu.values[2][:-1] is not None:
+                    await client.publish(TOPIC_RH_3, bmeu.values[2][:-1], retain=0, qos=0)
+                if bmeu.values[1][:-3] is not None:
+                    await client.publish(TOPIC_PRESSURE_3, bmeu.values[1][:-3], retain=0, qos=0)
+
 
 async def show_what_i_do():
     # Output is REPL
@@ -210,25 +257,35 @@ async def show_what_i_do():
             print("WiFi failed connects %s" % net.con_att_fail)
         if START_MQTT == 1:
             print("MQTT Connected %s" % mqtt_up)
-            print("MQTT broker uptime %s" % broker_uptime)
+            # print("MQTT broker uptime %s" % broker_uptime)
         print("Memory free: %s" % gc.mem_free())
         print("Memory alloc: %s" % gc.mem_alloc())
         print("-------")
-        print("Sensor I2C channel 1 0x76")
-        print("Temp: %s" % bmes.values[0][:-1])
-        print("Moisture: %s" % bmes.values[2][:-1])
-        print("Pressure: %s" % bmes.values[1][:-3])
-        print("-------")
-        print("Sensor I2C channel 2 0x76")
-        print("Temp: %s" % bmet.values[0][:-1])
-        print("Moisture: %s" % bmet.values[2][:-1])
-        print("Pressure: %s" % bmet.values[1][:-3])
-        print("-------")
-        print("Sensor I2C channel 2 0x77")
-        print("Temp: %s" % bmeu.values[0][:-1])
-        print("Moisture: %s" % bmeu.values[2][:-1])
-        print("Pressure: %s" % bmeu.values[1][:-3])
-        print("-------")
+        if sensor1faulty:
+            print("Sensor 1 faulty!")
+        else:
+            print("-------")
+            print("Sensor I2C channel 1 0x76")
+            print("Temp: %s" % bmes.values[0][:-1])
+            print("Moisture: %s" % bmes.values[2][:-1])
+            print("Pressure: %s" % bmes.values[1][:-3])
+        if sensor2faulty:
+            print("Sensor 2 faulty!")
+        else:
+            print("-------")
+            print("Sensor I2C channel 2 0x76")
+            print("Temp: %s" % bmet.values[0][:-1])
+            print("Moisture: %s" % bmet.values[2][:-1])
+            print("Pressure: %s" % bmet.values[1][:-3])
+        if sensor3faulty:
+            print("Sensor 3 faulty!")
+        else:
+            print("-------")
+            print("Sensor I2C channel 2 0x77")
+            print("Temp: %s" % bmeu.values[0][:-1])
+            print("Moisture: %s" % bmeu.values[2][:-1])
+            print("Pressure: %s" % bmeu.values[1][:-3])
+            print("-------")
         await asyncio.sleep(5)
 
 

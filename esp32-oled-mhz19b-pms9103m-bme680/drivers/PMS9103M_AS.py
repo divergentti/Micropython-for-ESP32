66c8f12b5496729d43962b60c835372674d07f94
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2024-08-17 15:35:42 +0300

    Update PMS9103M_AS.py

--- esp32-oled-mhz19b-pms9103m-bme680/drivers/PMS9103M_AS.py
+++ esp32-oled-mhz19b-pms9103m-bme680/drivers/PMS9103M_AS.py
@@ -1,3 +1,4 @@
+
 # 11.08.2024 - Jari Hiltunen
 
 from machine import UART
@@ -7,7 +8,7 @@ import utime
 import uasyncio as asyncio
 
 
-class PSensorPMS9103M:
+class PMS:
     START_BYTE_1 = 0x42
     START_BYTE_2 = 0x4d
     PMS_FRAME_LENGTH = 0
@@ -27,12 +28,6 @@ class PSensorPMS9103M:
     PMS_ERROR = 14
     PMS_CHECKSUM = 15
 
-    # Correctly calculate the checksum for the active mode command
-    @staticmethod
-    def calculate_checksum(command):
-        checksum = sum(command) & 0xFFFF  # ensure it's a 16-bit sum
-        return checksum
-
     async def writer(self, data):
         port = asyncio.StreamWriter(self.sensor, {})
         port.write(data)
@@ -47,15 +42,6 @@ class PSensorPMS9103M:
         self.startup_time = utime.time()
         self.read_interval = 30
         self.debug = False
-        wake_up_command = bytearray([0x42, 0x4d, 0xe4, 0x00, 0x01, 0x00, 0x00, 0x00])
-        checksum = self.calculate_checksum(wake_up_command)
-        self.PMS_WAKE_UP = wake_up_command + bytearray([checksum >> 8, checksum & 0xFF])
-        active_mode_command = bytearray([0x42, 0x4d, 0xe1, 0x00, 0x01, 0x00, 0x00, 0x00])
-        checksum = self.calculate_checksum(active_mode_command)
-        self.PMS_ACTIVE_MODE = active_mode_command + bytearray([checksum >> 8, checksum & 0xFF])
-        # Wake up and set active mode
-        self.writer(self.PMS_WAKE_UP)
-        self.writer(self.PMS_ACTIVE_MODE)
 
     async def reader(self, chars):
         self.last_read = utime.time()
@@ -83,39 +69,38 @@ class PSensorPMS9103M:
     async def read_async_loop(self):
 
         while True:
-
             if (utime.time() - self.last_read) < self.read_interval:
                 await asyncio.sleep(1)
             else:
                 first_byte = await self.reader(1)
-                if not self._assert_byte(first_byte, PSensorPMS9103M.START_BYTE_1):
+                if not self._assert_byte(first_byte, PMS.START_BYTE_1):
                     continue
                 second_byte = await self.reader(1)
-                if not self._assert_byte(second_byte, PSensorPMS9103M.START_BYTE_2):
+                if not self._assert_byte(second_byte, PMS.START_BYTE_2):
                     continue
                 # we are reading 30 bytes left
                 read_bytes = await self.reader(30)
                 if len(read_bytes) < 30:
                     continue
                 data = struct.unpack('!HHHHHHHHHHHHHBBH', read_bytes)
-                checksum = PSensorPMS9103M.START_BYTE_1 + PSensorPMS9103M.START_BYTE_2
+                checksum = PMS.START_BYTE_1 + PMS.START_BYTE_2
                 checksum += sum(read_bytes[:28])
-                if checksum != data[PSensorPMS9103M.PMS_CHECKSUM]:
+                if checksum != data[PMS.PMS_CHECKSUM]:
                     continue
                 self.pms_dictionary = {
-                    'FRAME_LENGTH': data[PSensorPMS9103M.PMS_FRAME_LENGTH],
-                    'PM1_0': data[PSensorPMS9103M.PMS_PM1_0],
-                    'PM2_5': data[PSensorPMS9103M.PMS_PM2_5],
-                    'PM10_0': data[PSensorPMS9103M.PMS_PM10_0],
-                    'PM1_0_ATM': data[PSensorPMS9103M.PMS_PM1_0_ATM],
-                    'PM2_5_ATM': data[PSensorPMS9103M.PMS_PM2_5_ATM],
-                    'PM10_0_ATM': data[PSensorPMS9103M.PMS_PM10_0_ATM],
-                    'PCNT_0_3': data[PSensorPMS9103M.PMS_PCNT_0_3],
-                    'PCNT_0_5': data[PSensorPMS9103M.PMS_PCNT_0_5],
-                    'PCNT_1_0': data[PSensorPMS9103M.PMS_PCNT_1_0],
-                    'PCNT_2_5': data[PSensorPMS9103M.PMS_PCNT_2_5],
-                    'PCNT_5_0': data[PSensorPMS9103M.PMS_PCNT_5_0],
-                    'PCNT_10_0': data[PSensorPMS9103M.PMS_PCNT_10_0],
-                    'VERSION': data[PSensorPMS9103M.PMS_VERSION],
-                    'ERROR': data[PSensorPMS9103M.PMS_ERROR],
-                    'CHECKSUM': data[PSensorPMS9103M.PMS_CHECKSUM], }
+                    'FRAME_LENGTH': data[PMS.PMS_FRAME_LENGTH],
+                    'PM1_0': data[PMS.PMS_PM1_0],
+                    'PM2_5': data[PMS.PMS_PM2_5],
+                    'PM10_0': data[PMS.PMS_PM10_0],
+                    'PM1_0_ATM': data[PMS.PMS_PM1_0_ATM],
+                    'PM2_5_ATM': data[PMS.PMS_PM2_5_ATM],
+                    'PM10_0_ATM': data[PMS.PMS_PM10_0_ATM],
+                    'PCNT_0_3': data[PMS.PMS_PCNT_0_3],
+                    'PCNT_0_5': data[PMS.PMS_PCNT_0_5],
+                    'PCNT_1_0': data[PMS.PMS_PCNT_1_0],
+                    'PCNT_2_5': data[PMS.PMS_PCNT_2_5],
+                    'PCNT_5_0': data[PMS.PMS_PCNT_5_0],
+                    'PCNT_10_0': data[PMS.PMS_PCNT_10_0],
+                    'VERSION': data[PMS.PMS_VERSION],
+                    'ERROR': data[PMS.PMS_ERROR],
+                    'CHECKSUM': data[PMS.PMS_CHECKSUM], }

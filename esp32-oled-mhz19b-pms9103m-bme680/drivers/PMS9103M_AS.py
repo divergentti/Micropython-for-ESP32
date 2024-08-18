9fed772f6ce809700beaf2be6c5c187d1118e6d1
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2024-08-18 16:02:49 +0300

    Update PMS9103M_AS.py

--- esp32-oled-mhz19b-pms9103m-bme680/drivers/PMS9103M_AS.py
+++ esp32-oled-mhz19b-pms9103m-bme680/drivers/PMS9103M_AS.py
@@ -1,146 +1,113 @@
+"""
+  19.01.2020: Jari Hiltunen
+
+  Original https://github.com/pkucmus/micropython-pms7003/blob/master/pms7003.py
+  Modified for asyncronous StreamReader  16.01.2020 by Divergentti / Jari Hiltunen
+
+  Add loop into your code loop.create_task(objectname.read_async_loop())
+
+"""
+
 from machine import UART, Pin
+import gc
+import struct
 import utime
 import uasyncio as asyncio
 
+
 class PMS:
-    # Write commands:
-    # Start Byte 1 Start Byte 2 Command Data 1 Data 2     Verify Byte 1 Verify Byte 2
-    # 0x42         0x4d         CMD     DATAH  DATAL      LRCH          LRCL
-    # 0x42         0x4d         0xe2    0x00   0x00                              = Read in passive mode
-    # 0x42         0x4d         0xe1    0x00   0x00/0x01                         = 0x00 = passive, 0x01 = active
-    # 0x42         0x4d         0xe4    0x00   0x00/0x01                         = 0x00 = sleep, 0x01 = wakeup
-    # Commands without CRC bytes
-    CMD_WAKEUP = bytearray([0x42, 0x4d, 0xe4, 0x00, 0x01])
-    CMD_SLEEP = bytearray([0x42, 0x4d, 0xe4, 0x00, 0x00])
-    CMD_ACTIVE_MODE = bytearray([0x42, 0x4d, 0xe1, 0x00, 0x01])
-    CMD_PASSIVE_MODE = bytearray([0x42, 0x4d, 0xe1, 0x00, 0x00])
-    # Read passive command 0xe2 returns 32 bytes with PCNT and PM-values
-    CMD_READ_PASSIVE = bytearray([0x42, 0x4d, 0xe2, 0x00, 0x00])
 
     START_BYTE_1 = 0x42
     START_BYTE_2 = 0x4d
-    PM1_OFFSET = 6
-    PM2P5_OFFSET = 8
-    PM10_OFFSET = 10
-    PM1_ATM_OFFSET = 12
-    PM2P5_ATM_OFFSET = 14
-    PM10_ATM_OFFSET = 16
-    PCNT0_3_OFFSET = 18
-    PCNT0_5_OFFSET = 20
-    PCNT1_0_OFFSET = 22
-    PCNT2_5_OFFSET = 24
-    PCNT5_0_OFFSET = 26
-    PCNT10_OFFSET = 28
-    VERSION_OFFSET = 30
-    ERROR_OFFSET = 32
-    MAX_DATA_LENGTH = 32  # Adjusted to 32 as per CMD_READ_PASSIVE length
-    READ_INTERVAL = 10
+    PMS_FRAME_LENGTH = 0
+    PMS_PM1_0 = 1
+    PMS_PM2_5 = 2
+    PMS_PM10_0 = 3
+    PMS_PM1_0_ATM = 4
+    PMS_PM2_5_ATM = 5
+    PMS_PM10_0_ATM = 6
+    PMS_PCNT_0_3 = 7
+    PMS_PCNT_0_5 = 8
+    PMS_PCNT_1_0 = 9
+    PMS_PCNT_2_5 = 10
+    PMS_PCNT_5_0 = 11
+    PMS_PCNT_10_0 = 12
+    PMS_VERSION = 13
+    PMS_ERROR = 14
+    PMS_CHECKSUM = 15
 
-    @staticmethod
-    def calculate_checksum(command):
-        checksum = sum(command) & 0xFFFF
-        lrch = (checksum >> 8) & 0xFF
-        lrcl = checksum & 0xFF
-        return lrch, lrcl
-
-    def verify_checksum(self, frame):
-        checksum = sum(frame[:-2]) & 0xFFFF
-        return (frame[-2] << 8 | frame[-1]) == checksum
-
-    async def writer(self, data):
-        self.sensor.write(data)
-        await asyncio.sleep(0.1)
-
-    def __init__(self, rxpin=32, txpin=33, uart=1):
+    #  Default UART1, rx=32, tx=33. Don't use UART0 if you want to use REPL!
+    def __init__(self, rxpin=16, txpin=17, uart=2):
         self.sensor = UART(uart, baudrate=9600, bits=8, parity=None, stop=1, rx=Pin(rxpin), tx=Pin(txpin))
-        self.buffer = bytearray(self.MAX_DATA_LENGTH)
         self.pms_dictionary = None
-        self.last_read = 0
         self.startup_time = utime.time()
+        self.read_time = 0
         self.read_interval = 30
         self.debug = False
-        asyncio.run(self.wake_up())
-        asyncio.run(self.enter_passive_mode())
 
-    async def read_frame(self):
-        while True:
-            while self.sensor.any() < 2:
-                await asyncio.sleep(0.1)
-            if self.sensor.read(1)[0] == self.START_BYTE_1 and self.sensor.read(1)[0] == self.START_BYTE_2:
-                break
-
-        remaining_bytes = self.MAX_DATA_LENGTH - 2
-        self.buffer[0] = self.START_BYTE_1
-        self.buffer[1] = self.START_BYTE_2
-        self.sensor.readinto(self.buffer[2:], remaining_bytes)
-
-        if self.debug:
-            print("PMS buffer: %s" % self.buffer)
-        return self.buffer
-
-    async def read_pm_values(self):
-        checksum_high, checksum_low = self.calculate_checksum(self.CMD_READ_PASSIVE)
-        await self.writer(self.CMD_READ_PASSIVE + bytearray([checksum_high, checksum_low]))
-        frame = await self.read_frame()
-
-        if self.verify_checksum(frame):
-            pm1 = frame[self.PM1_OFFSET] << 8 | frame[self.PM1_OFFSET + 1]
-            pm1_atm = frame[self.PM1_ATM_OFFSET] << 8 | frame[self.PM1_ATM_OFFSET + 1]
-            pm2_5 = frame[self.PM2P5_OFFSET] << 8 | frame[self.PM2P5_OFFSET + 1]
-            pm2_5_atm = frame[self.PM2P5_ATM_OFFSET] << 8 | frame[self.PM2P5_ATM_OFFSET + 1]
-            pm10 = frame[self.PM10_OFFSET] << 8 | frame[self.PM10_OFFSET + 1]
-            pm10_atm = frame[self.PM10_ATM_OFFSET] << 8 | frame[self.PM10_ATM_OFFSET + 1]
-            pcnt0_3 = frame[self.PCNT0_3_OFFSET] << 8 | frame[self.PCNT0_3_OFFSET + 1]
-            pcnt0_5 = frame[self.PCNT0_5_OFFSET] << 8 | frame[self.PCNT0_5_OFFSET + 1]
-            pcnt1_0 = frame[self.PCNT1_0_OFFSET] << 8 | frame[self.PCNT1_0_OFFSET + 1]
-            pcnt2_5 = frame[self.PCNT2_5_OFFSET] << 8 | frame[self.PCNT2_5_OFFSET + 1]
-            pcnt5_0 = frame[self.PCNT5_0_OFFSET] << 8 | frame[self.PCNT5_0_OFFSET + 1]
-            pcnt10 = frame[self.PCNT10_OFFSET] << 8 | frame[self.PCNT10_OFFSET + 1]
-            version = frame[self.VERSION_OFFSET] << 8 | frame[self.VERSION_OFFSET + 1]
-            error = frame[self.ERROR_OFFSET] << 8 | frame[self.ERROR_OFFSET + 1]
-            self.last_read = utime.time()
-            self.pms_dictionary = {
-                'PM1_0': pm1,
-                'PM2_5': pm2_5,
-                'PM10_0': pm10,
-                'PM1_0_ATM': pm1_atm,
-                'PM2_5_ATM': pm2_5_atm,
-                'PM10_0_ATM': pm10_atm,
-                'PCNT_0_3': pcnt0_3,
-                'PCNT_0_5': pcnt0_5,
-                'PCNT_1_0': pcnt1_0,
-                'PCNT_2_5': pcnt2_5,
-                'PCNT_5_0': pcnt5_0,
-                'PCNT_10_0': pcnt10,
-                'VERSION': version,
-                'ERROR': error
-            }
-            self.last_read = utime.time()
-            return True
-        else:
+    async def reader(self, chars):
+        port = asyncio.StreamReader(self.sensor)
+        try:
+            data = await port.readexactly(chars)
             if self.debug:
-                print("Error: Incomplete data frame.")
+                print("PMS data in %s" % data)
+            if len(data) == int(chars):
+                return data
+            else:
+                return False
+        except MemoryError:
+            gc.collect()
             return False
 
-    async def wake_up(self):
-        checksum_high, checksum_low = self.calculate_checksum(self.CMD_WAKEUP)
-        await self.writer(self.CMD_WAKEUP + bytearray([checksum_high, checksum_low]))
-
-    async def sleep(self):
-        checksum_high, checksum_low = self.calculate_checksum(self.CMD_SLEEP)
-        await self.writer(self.CMD_SLEEP + bytearray([checksum_high, checksum_low]))
-
-    async def enter_passive_mode(self):
-        checksum_high, checksum_low = self.calculate_checksum(self.CMD_PASSIVE_MODE)
-        await self.writer(self.CMD_PASSIVE_MODE + bytearray([checksum_high, checksum_low]))
-
-    async def enter_active_mode(self):
-        checksum_high, checksum_low = self.calculate_checksum(self.CMD_ACTIVE_MODE)
-        await self.writer(self.CMD_ACTIVE_MODE + bytearray([checksum_high, checksum_low]))
+    @staticmethod
+    def _assert_byte(byte, expected):
+        if byte is None or len(byte) < 1 or ord(byte) != expected:
+            return False
+        return True
 
     async def read_async_loop(self):
+
         while True:
-            status = await self.read_pm_values()
+
+            first_byte = await self.reader(1)
+            if not self._assert_byte(first_byte, PMS.START_BYTE_1):
+                continue
+
+            second_byte = await self.reader(1)
+            if not self._assert_byte(second_byte, PMS.START_BYTE_2):
+                continue
+
+            # we are reading 30 bytes left
+            read_bytes = await self.reader(30)
+            if len(read_bytes) < 30:
+                continue
+
+            data = struct.unpack('!HHHHHHHHHHHHHBBH', read_bytes)
+
+            checksum = PMS.START_BYTE_1 + PMS.START_BYTE_2
+            checksum += sum(read_bytes[:28])
+
+            if checksum != data[PMS.PMS_CHECKSUM]:
+                continue
+
+            self.pms_dictionary = {
+                'FRAME_LENGTH': data[PMS.PMS_FRAME_LENGTH],
+                'PM1_0': data[PMS.PMS_PM1_0],
+                'PM2_5': data[PMS.PMS_PM2_5],
+                'PM10_0': data[PMS.PMS_PM10_0],
+                'PM1_0_ATM': data[PMS.PMS_PM1_0_ATM],
+                'PM2_5_ATM': data[PMS.PMS_PM2_5_ATM],
+                'PM10_0_ATM': data[PMS.PMS_PM10_0_ATM],
+                'PCNT_0_3': data[PMS.PMS_PCNT_0_3],
+                'PCNT_0_5': data[PMS.PMS_PCNT_0_5],
+                'PCNT_1_0': data[PMS.PMS_PCNT_1_0],
+                'PCNT_2_5': data[PMS.PMS_PCNT_2_5],
+                'PCNT_5_0': data[PMS.PMS_PCNT_5_0],
+                'PCNT_10_0': data[PMS.PMS_PCNT_10_0],
+                'VERSION': data[PMS.PMS_VERSION],
+                'ERROR': data[PMS.PMS_ERROR],
+                'CHECKSUM': data[PMS.PMS_CHECKSUM], }
+            self.read_time = utime.time()
             if self.debug:
-                print("PMS read status: %s" % status)
-            await asyncio.sleep(self.READ_INTERVAL)
+                print("PMS Read at %s" % self.read_time)
+            await asyncio.sleep(self.read_interval)
